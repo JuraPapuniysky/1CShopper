d3e8b7f715203f979c080e9dc6bb1316bfc1f38c
JuraPapuniysky <jurapapunitsky@gmail.com>
2017-11-21 08:06:36 +0200

    AccessControll for backend

--- backend/views/news-promotion/_form.php
+++ backend/views/news-promotion/_form.php
@@ -16,12 +16,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
+    <?= $form->field($model, 'text')->widget(\dosamigos\ckeditor\CKEditor::className()) ?>
+    
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
